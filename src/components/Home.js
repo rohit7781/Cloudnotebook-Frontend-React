3044bf0d81d3f67ca6b25629089eee2343785200
Rohit Kumar <rohitkumar703314@gmail.com>
2021-12-21 17:48:41 +0530

    Added home and routes

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+// import Notes from './Notes';
+
+export const Home = () => {
+
+    return (
+        <div> 
+            {/* <Notes/> */}
+            <h1>Thsi is notes</h1>
+        </div>
+    )
+}
