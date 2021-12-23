d2e9acd1ad1191ffd21a10a4176c4c921590a8ae
Rohit Kumar <rohitkumar703314@gmail.com>
2021-12-24 02:16:12 +0530

    Added Login

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Navbar from './components/Navbar';
 import { Home } from './components/Home';
 import About from './components/About';
 import Notestate from './context/notes/NoteState';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
     <Routes>
     <Route exact path="/" element={<Home/>} />
     <Route exact path="/about" element={<About/>} />
+    <Route exact path="/login" element={<Login/>} />
+    <Route exact path="/signup" element={<Signup/>} />
 
     </Routes>
     </div>
